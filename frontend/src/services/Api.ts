dcd0e18941d9189fde5e6bea0906b7f4de1c7af0
simranchauhan13 <simranchauhan1394@gmail.com>
2026-07-30 16:14:26 +0530

    Update production API URL

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-});
+ baseURL: "https://predictwell-7qik.onrender.com"});
 
 export default api;
